a723f34319c2e0c9be024b0b9c8dcba7e728b13b
monkphin <darren.burrows@gmail.com>
2024-07-18 14:27:29 +0100

    added and tested basic name capture functionality

---
+++ assets/js/javascript.js
@@ -0,0 +1,27 @@
+//----------------------------------------- Global Vars
+
+
+//-----------------------------------------Player Information 
+document.getElementById('name_form').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    let playerName = document.getElementById('player_name').value;
+    console.log(playerName); 
+});
+
+//-----------------------------------------Deck information 
+
+
+//-----------------------------------------Game State
+
+
+//-----------------------------------------Messaging 
+
+
+//-----------------------------------------Stats
+
+
+
+
+
+
+
