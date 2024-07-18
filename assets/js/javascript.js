b4babe71251254748ab67aaafc158409c91453e2
monkphin <darren.burrows@gmail.com>
2024-07-18 15:04:05 +0100

    refactored and fleshed out playerName functionality to use LocalStorage and load the game html

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,12 +1,71 @@
 //----------------------------------------- Global Vars
 
 
-//-----------------------------------------Player Information 
-document.getElementById('name_form').addEventListener('submit', function(event) {
-    event.preventDefault(); 
-    let playerName = document.getElementById('player_name').value;
-    console.log(playerName); 
+
+//-----------------------------------------Game Start
+/**
+ * Check to ensure the DOM is loaded.
+ * This will ensure the form is initialised to listen for events
+ * various functions required for the app are also being triggered here. 
+ */
+
+document.addEventListener('DOMContentLoaded', function() {
+  if (document.getElementById('welcomeMessage')) {
+    titleName();
+  }
+
+  const form = document.getElementById('name_form');
+  if (form) {
+    form.addEventListener('submit', handleSubmit);
+  };
+  displayPlayerName();
 });
+
+//-----------------------------------------Player Information 
+
+
+
+function handleSubmit(e) {
+  e.preventDefault();
+  let name = document.getElementById('player_name').value;
+  
+  //check to ensure name has been entered
+  if(name) {
+    storePlayerName(name);
+    window.location.assign('game.html');
+  } else {
+    alert('Please enter your name before continuing');
+  }
+};
+  
+  /**
+   * 
+   * The below functions store and retrain the player name, using localstorage.
+   *  https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+   */
+function storePlayerName(name) {
+  localStorage.setItem('playerName', name);
+};
+  
+function retrievePlayerName() {
+  let playerName = localStorage.getItem('playerName');
+  return playerName;
+}
+  
+/** 
+ * Throws a welcome message to the game.html page when loaded
+ * this present sthe players name retrieved from local storage. 
+ */
+
+function displayPlayerName() {
+  let playerName = retrievePlayerName();
+  document.getElementById('welcome_message').innerHTML += 
+  `
+  <h1>Welcome to PokeBattle ${playerName}</h1>
+  <p>Good luck with your game!</p>
+  `;
+}
+
 
 //-----------------------------------------Deck information 
 
@@ -20,7 +79,9 @@ document.getElementById('name_form').addEventListener('submit', function(event) 
 //-----------------------------------------Stats
 
 
-
+//-----------------------------------------Testing Stuff
+//Name handling 
+// console.log(playerName);
 
 
 
